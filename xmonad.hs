fb1e0f91ae69b24c1498a0c5a89f0346ecca6435
Maximiliano Pin <mxcpin@gmail.com>
2019-02-18 12:19:20 +0100

    Key binding for skype

--- xmonad.hs
+++ xmonad.hs
@@ -212,6 +212,7 @@ myKeyBindings =
     , ((0, 0x1008FF12), spawn "amixer -q set Master toggle")
     , ((0, 0x1008FF11), spawn "amixer -q set Master 10%-")
     , ((0, 0x1008FF13), spawn "amixer -q set Master 10%+")
+    , ((mod1Mask .|. shiftMask, xK_s), spawn "skypeforlinux")
   ]
 
 
